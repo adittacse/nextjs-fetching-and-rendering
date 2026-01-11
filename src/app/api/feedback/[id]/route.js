942004dff5f37f181d781d56f27087e21def6658
Aditta Chakraborty <cseaditta@gmail.com>
2026-01-12 02:22:32 +0600

    feedback CRUD api's created

---
+++ src/app/api/feedback/[id]/route.js
@@ -0,0 +1,65 @@
+import { connect } from "@/app/lib/dbConnect";
+import { ObjectId } from "mongodb";
+
+const feedbackCollection = connect("feedbacks");
+
+// get single feedback
+export async function GET(request, { params }) {
+    const { id } = await params;
+
+    if (id.length !== 24) {
+        return Response.json({
+            status: 400,
+            message: "Invalid id"
+        });
+    }
+
+    const query = { _id: new ObjectId(id) };
+    const result = await feedbackCollection.findOne(query);
+    return Response.json(result);
+}
+
+// update single feedback
+export async function PATCH(request, { params }) {
+    const { id } = await params;
+    const { message } = await request.json();
+
+    if (id.length !== 24) {
+        return Response.json({
+            status: 400,
+            message: "Invalid id"
+        });
+    }
+
+    if (!message || typeof message !== "string") {
+        return Response.json({
+            status: 400,
+            message: "please send a message!",
+        });
+    }
+
+    const query = { _id: new ObjectId(id) };
+    const newData = {
+        $set: {
+            message,
+        }
+    }
+    const result = await feedbackCollection.updateOne(query, newData);
+    return Response.json(result);
+}
+
+// delete single feedback
+export async function DELETE(request, { params }) {
+    const { id } = await params;
+
+    if (id.length !== 24) {
+        return Response.json({
+            status: 400,
+            message: "Invalid id"
+        });
+    }
+
+    const query = { _id: new ObjectId(id) };
+    const result = await feedbackCollection.deleteOne(query);
+    return Response.json(result);
+}
